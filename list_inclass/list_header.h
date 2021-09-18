b53fbc2409c2d21fcc5979ddb0092fd4dcd9cb2d
SourVoice <2985122798@qq.com>
2021-09-18 16:35:58 +0800

    简单链表设计作业完成

--- list_inclass/list_header.h
+++ list_inclass/list_header.h
@@ -7,52 +7,64 @@ typedef struct LINK
 } Link;
 #define TRUE 1
 #define FALSE 0
-struct LINK *creat_list(int total_elements)
+Link *creat_list(int total_elements)
 {
-    struct LINK *head;
-    head = (struct LINK *)malloc(sizeof(struct LINK));
-    struct LINK *current = head;
-    struct LINK *newnode = NULL;
+    Link *head;
+    head = (Link *)malloc(sizeof(Link));
+    Link *current = head;
+    Link *newNode = NULL;
+    printf("依次输入元素：");
     for (int i = 0; i < total_elements; i++)
     {
-        newnode = (struct LINK *)malloc(sizeof(struct LINK));
-        if (newnode == NULL)
+        newNode = (Link *)malloc(sizeof(Link));
+        if (newNode == NULL)
             return NULL;
-        scanf("%d", &newnode->value);
-        newnode->next = NULL;
-        current->next = newnode;
-        current = newnode;
+        scanf("%d", &newNode->value);
+        newNode->next = NULL;
+        current->next = newNode;
+        current = newNode;
     }
     return head->next;
 }
-int sll_insert(LINK *current, int new_value) //将新的结点信息传递进函数
+int insert_by_value(Link *current, int new_value) //将新的结点信息传递进函数
 {
-    LINK *previous;
-    LINK *new_node;
+    Link *previous;
+    Link *newNode;
     while (current != NULL & current->value < new_value) //previous指向小于current的上一个结点,不断向前移动
     {
         previous = current;
         current = current->next;
     }
 
-    new_node = (LINK *)malloc(sizeof(LINK));
-    if (new_node == NULL) //分配失败
+    newNode = (Link *)malloc(sizeof(Link));
+    if (newNode == NULL) //分配失败
         return FALSE;
-    /*并没有改变结构体内存,仅仅是创建了一个新指针,从previous的结点跳出执行new结点,new里的link再指向下一个＞它的结点*/
-    new_node->value = new_value;
-    new_node->next = current;
-    previous->next = new_node;
+    /*并没有改变结构体内存,仅仅是创建了一个新指针,从previous的结点跳出执行new结点,new里的Link再指向下一个＞它的结点*/
+    newNode->value = new_value;
+    newNode->next = current;
+    previous->next = newNode;
     return TRUE;
 }
-LINK *insert(LINK **rootp, LINK *new_node, int pos)
+int insert_by_pos(Link **rootp, int pos, int val)
 {
-    LINK *current = NULL;
-    LINK *head = NULL;
-    for (int i;;)
+    Link *current = *rootp;
+    Link *newNode = NULL;
+    current = *rootp;
+
+    for (int i = 1; current != NULL && i < pos; i++)
     {
+        rootp = &current->next;
+        current = *rootp;
     }
+    newNode = (Link *)malloc(sizeof(Link));
+    if (newNode == NULL)
+        return 0;
+    newNode->value = val;
+    newNode->next = current;
+    *rootp = newNode;
+    return 1;
 }
-Link *delet(Link **rootp, int target_value)
+Link *delet_by_value(Link **rootp, int target_value)
 {
     Link *current = NULL;
     Link *head = NULL;
@@ -60,7 +72,7 @@ Link *delet(Link **rootp, int target_value)
     current = *rootp;
     rootp = &current->next;
     if (*rootp == NULL)
-        return;
+        return NULL;
 
     while (current != NULL)
     {
@@ -87,4 +99,56 @@ Link *delet(Link **rootp, int target_value)
         current = *rootp;
     }
     return head;
+}
+Link *delet_by_pos(Link **rootp, int pos)
+{
+    if (*rootp == NULL)
+        return NULL;
+
+    Link *current = *rootp;
+    Link *head = *rootp;
+
+    int i = 1;
+    if (pos == 1)
+    {
+        head = current->next;
+        free(current);
+        return head;
+    }
+    while (current != NULL)
+    {
+        if (i == pos)
+        {
+            break;
+        }
+        rootp = &current->next;
+        current = *rootp;
+        i++;
+    }
+    *rootp = current->next;
+    free(current);
+    current = *rootp;
+
+    return head;
+}
+void print_list(Link *list)
+{
+    Link *current = list;
+    while (current != NULL)
+    {
+        printf("%d ", current->value);
+        current = current->next;
+    }
+    printf("\n");
+}
+void freeList(Link **rootp)
+{
+    Link *current = *rootp;
+
+    while (current != NULL)
+    {
+        rootp = &current->next;
+        free(current);
+        current = *rootp;
+    }
 }
