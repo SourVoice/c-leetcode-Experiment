9b9e4402ae06bc2865ab67fe251d6cf709a3bab6
SourVoice <2985122798@qq.com>
2021-10-25 19:36:51 +0800

    版本回退

---
+++ Experimentation Six Stack Implementation and Expression Calculation/judgeFunction.h
@@ -0,0 +1,33 @@
+
+#define op_left_assoc(c) (c == '+' || c == '-' || c == '/' || c == '*' || c == '%') /*左结合*/
+#define is_operator(c) (c == '+' || c == '-' || c == '/' || c == '*' || c == '!' || c == '%' || c == '=')
+#define is_function(c) (c >= 'A' && c <= 'Z')
+#define is_ident(c) ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z'))
+#define false 0
+#define true 1
+
+// 操作符
+// 优先级	符号	运算顺序
+// 1		!		从右至左
+// 2		* / %	从左至右
+// 3		+ -		从左至右
+// 4		=		从右至左
+int op_preced(const char c)
+{
+    switch (c)
+    {
+    case '!':
+        return 4;
+    case '*':
+    case '/':
+    case '%':
+        return 3;
+    case '+':
+    case '-':
+        return 2;
+    case '=':
+        return 1;
+    }
+    //若输入不是运算符
+    return 0;
+}
---
+++ Experimentation Six Stack Implementation and Expression Calculation/stack.h
@@ -0,0 +1,25 @@
+#ifndef STACK_H
+#define STACK_H
+#endif
+int top = -1;
+int size = 32;
+void push(char *stack, char data)
+{
+    stack[++top] = data;
+}
+char pop(char *stack)
+{
+    return stack[top--];
+}
+char Top(const char *stack)
+{
+    return stack[top];
+}
+int isEmpty(char *stack)
+{
+    return top == -1;
+}
+int isFull(char *stack)
+{
+    return top == size - 1;
+}
