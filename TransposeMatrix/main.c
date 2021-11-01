3005d15da6538ba54c323833c535c824f8442d91
SourVoice <2985122798@qq.com>
2021-11-01 15:50:05 +0800

    增加add功能

--- TransposeMatrix/main.c
+++ TransposeMatrix/main.c
@@ -31,9 +31,27 @@ int main()
         if (choose == 2)
         {
             int toTrans;
+            int transChoose = 1;
             printf("input the NO of the Matrix to Trans: ");
             scanf("%d", &toTrans);
-            TransposeSMatrixA(*matrix.MatrixNum[toTrans], matrix.MatrixNum[NO]);
+            printf("please choose in which way you want to trans: \n");
+            while (transChoose)
+            {
+                printf("input 1:  normal way trans\n");
+                printf("input 2:  faster way trans\n");
+                scanf("%d", &transChoose);
+
+                if (transChoose == 1)
+                {
+                    TransposeSMatrixA(*matrix.MatrixNum[toTrans], matrix.MatrixNum[NO]); //the normal way to trans
+                    break;
+                }
+                else if (transChoose == 2)
+                {
+                    FastTransposeSMatrix(*matrix.MatrixNum[toTrans], matrix.MatrixNum[NO]); //the faster way(not change the function's )
+                    break;
+                }
+            }
             printf("The matrix have been tranposed, output Matrix's NO is %d\n", NO);
             displayMatrix(*matrix.MatrixNum[NO]);
             NO++;
@@ -44,7 +62,7 @@ int main()
             printf("input the NO of two Matrix which you want to multi: ");
             scanf("%d %d", &toMulti1, &toMulti2);
             MultSMatrix(*matrix.MatrixNum[toMulti1], *matrix.MatrixNum[toMulti2], matrix.MatrixNum[NO]);
-            printf("Matrix.NO %d and Matrix.NO %d have been added, output Matrix's NO is %d\n", toMulti1, toMulti2, NO);
+            printf("Matrix.NO %d and Matrix.NO %d have been multied     \ntheoutput Matrix's NO is %d\n", toMulti1, toMulti2, NO);
             displayMatrix(*matrix.MatrixNum[NO]);
             NO++;
         }
@@ -54,6 +72,7 @@ int main()
             printf("input the NO of two Matrix which you want to add: ");
             scanf("%d %d", &toAdd1, &toAdd2);
             AddMaxtrix(*matrix.MatrixNum[toAdd1], *matrix.MatrixNum[toAdd2], matrix.MatrixNum[NO]);
+            printf("Matrix.NO %d and Matrix.NO %d have been added       \nthe output Matrix's NO is %d\n", toAdd1, toAdd2, NO);
             displayMatrix(*matrix.MatrixNum[NO]);
             NO++;
         }
