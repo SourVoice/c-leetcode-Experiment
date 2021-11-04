28d9daeb3ddac111fe3a9ca18f7a008504f93045
SourVoice <2985122798@qq.com>
2021-11-04 20:31:59 +0800

    因子法,二分法,函数法

--- leetcode/367. Valid Perfect Square.c
+++ leetcode/367. Valid Perfect Square.c
@@ -6,8 +6,39 @@ bool isPerfectSquare(int num)
 {
     return (int)sqrt(num) * (int)sqrt(num) == num ? 1 : 0;
 }
+bool isPerfectSquare_Binary(int num) //Binary,二分法
+{
+    int bottom = 1, top = num;
+    while (bottom <= top)
+    {
+        int mid = (top - bottom) / 2 + bottom;
+        int tmp = num / mid;
+
+        if (tmp > mid)
+            bottom = mid + 1;
+        else if (tmp < mid)
+            top = mid - 1;
+        else
+        {
+            if (num % mid == 0)
+                return 1;
+            bottom = mid + 1;
+        }
+    }
+    return 0;
+}
+bool isPerfectSquare_EnumerateFactor(int num) //找出一个num的因子,满足i*i=num
+{
+    for (int i = 1; i <= num / i; i++)
+    {
+        if (num == i * i)
+            return 1;
+    }
+    return 0;
+}
+
 int main()
 {
-    printf("%d", isPerfectSquare(14));
+    printf("%d", isPerfectSquare_Binary(14));
     return 0;
 }
