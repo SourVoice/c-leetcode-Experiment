13731b3998ec20a9eafef5b6ea6d6fdaf1fa3d01
SourVoice <2985122798@qq.com>
2021-11-05 20:17:39 +0800

    快慢指针函数修改

--- list/Singly_linked_list/快慢指针查中点.h
+++ list/Singly_linked_list/快慢指针查中点.h
@@ -5,11 +5,11 @@ Link *find_mid(Link *head)
 {
     Link *slow = head;
     Link *fast = head;
-    while (slow && fast->next)
+    while (fast && fast->next)
     {
         slow = slow->next;
         fast = fast->next->next;
     }
-    return slow;
+    return slow; /*当fast不为空时,结点个数为奇数个,否则为偶数个,且slow偏后*/
     /*1:2速度走遍链表,fast到达终点时,slow到达中点或偏后一个*/
 }
