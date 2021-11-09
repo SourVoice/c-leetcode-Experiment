4e12e121804ed1a6da9ae24541f7b5bb364d7124
SourVoice <2985122798@qq.com>
2021-11-09 23:24:45 +0800

    update size func

--- tree/Tree_Traversal_Inorder_Preorder_Postorder.c
+++ tree/Tree_Traversal_Inorder_Preorder_Postorder.c
@@ -6,7 +6,7 @@ struct node
     struct node *left;
     struct node *right;
 };
-struct node *newNode(int val)
+struct node *newNode(int val) //init a newnode of the tree
 {
     struct node *Node = (struct node *)malloc(sizeof(struct node));
     Node->val = val;
@@ -15,29 +15,74 @@ struct node *newNode(int val)
 
     return Node;
 }
-void printInOrder(struct node *tree)
+int size(struct node *root) //get size of the tree
 {
-    if (!tree)
-        return;
-    printInOrder(tree->left);
-    printf("%d ", tree->val);
-    printInOrder(tree->right);
+    if (!root)
+        return 0;
+    return size(root->left) + size(root->right) + 1;
 }
-void printPreOrder(struct node *tree)
+void LayerOrder(struct node *root) //层序遍历
 {
-    if (!tree)
-        return;
-    printf("%d", tree->val);
-    printPreOrder(tree->left);
-    printPreOrder(tree->right);
+    int treeSize = size(root);
+    int *a = malloc(sizeof(int) * treeSize);
+    int *strpos = a;
+    int *strend = a + treeSize;
+    if (root->left)
+    {
+        a[*strpos] = root->left->val;
+    }
+    else if (root->right)
+    {
+    }
 }
-void printPostOrder(struct node *tree)
+//====================================recursion递归法===================================================
+void printInOrder(struct node *root)
 {
-    if (!tree)
+    if (!root)
         return;
-    printPostOrder(tree->left);
-    printPostOrder(tree->right);
-    printf("%d ", tree->val);
+    printInOrder(root->left);
+    printf("%d ", root->val);
+    printInOrder(root->right);
+}
+void printPreOrder(struct node *root)
+{
+    if (!root)
+        return;
+    printf("%d ", root->val);
+    printPreOrder(root->left);
+    printPreOrder(root->right);
+}
+void printPostOrder(struct node *root)
+{
+    if (!root)
+        return;
+    printPostOrder(root->left);
+    printPostOrder(root->right);
+    printf("%d ", root->val);
+}
+//=======================================interative迭代法==============================================
+void printinorderInterative(struct node *root)
+{
+    int treesize = size(root);
+    struct node **stack = (struct node **)malloc(sizeof(struct node *) * treesize);
+    int top = 0;
+    while (root != NULL || top != 0)
+    {
+        while (root != NULL)
+        {
+            stack[top++] = root;
+            root = root->left;
+        }
+        root = stack[--top];
+        printf("%d ", root->val);
+        root = root->right;
+    }
+}
+void printpreorderInterative(struct node *root)
+{
+    int treesize = size(root);
+    struct node **stack = (struct node **)malloc(sizoef(struct node *) * treesize);
+    int top = 0;
 }
 int main()
 {
@@ -46,6 +91,9 @@ int main()
     root->right = newNode(3);
     root->left->left = newNode(4);
     root->left->right = newNode(5);
+
+    //recursion way
+    printf("\nTrabersal a Binary Tree in Recursion Way\n");
 
     printf("\nPreorder traversal of binary tree is \n");
     printPreOrder(root);
@@ -56,5 +104,29 @@ int main()
     printf("\nPostorder traversal of binary tree is \n");
     printPostOrder(root);
 
+    //interative way
+    printf("\nTrabersal a Binary Tree in Interative Way\n");
+
+    printf("\nInorder traversal of binary tree is \n");
+    printinorderInterative(root);
+
     return 0;
 }
+/*
+         1
+       /  \
+       2   3
+      / \ 
+     4   5
+由已知先序遍历和中序构造二叉树
+pre:A   B   D   E   G   H   C   F
+    r   
+        r
+            r
+                r
+in :D   B   G   E   H   A   C   F
+                        r
+    |       右      |       | 左 |
+        r
+   [右]     [   左  ]
+*/
