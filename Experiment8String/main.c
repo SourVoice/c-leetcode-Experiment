bd2511eabb63b02e151c839756144beaffd22f40
SourVoice <2985122798@qq.com>
2021-11-11 01:16:52 +0800

    coderunner成功但debugger不成功

--- Experiment8String/main.c
+++ Experiment8String/main.c
@@ -1,22 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main()
 {
 
-    char wrd[256], buffer[256];
-    int n, m, i, j, line;
-    int count = 0;
+    char *wrd[256];
+    char buffer[256];
+    int n, m, i, j, k = 0, line;
 
-    FILE *fp;
-    fp = fopen("text.txt", "r"); // open file
+    for (int i = 0; i < 256; i++)
+        wrd[i] = (char *)malloc(sizeof(char) * 256);
 
-    printf("Enter the word you want to search in the file: ");
-    gets(wrd);
+    FILE *fp = NULL;
+    fp = fopen("test.txt", "r"); // open file
 
-    m = strlen(wrd); // length of input word
-
-    printf("All positions of word \"%s\" in the file\n", wrd);
+    printf("Enter the words you want to search in the file and press ctrl+Z( ^Z ) to end input: \n");
+    while (gets(wrd[k++]))
+    {
+    }
+    int *count = (int *)malloc(sizeof(int) * (--k));
+    memset(count, 0, k);
+    for (int i = 0; i < k; i++)
+    {
+        count[i] = 0;
+    }
 
     line = 0;
     // the following loop the file fp line by line
@@ -33,19 +41,25 @@ int main()
         {
 
             // comparing current word with input word
-            j = 0;
-            while (i < n && j < m && buffer[i] == wrd[j])
+            for (int index = 0; index < k; index++)
             {
-                ++i, ++j;
-            }
+                char *word = wrd[index];
+                j = 0;
+                m = strlen(word); // length of input word
+                while (i < n && j < m && buffer[i] == word[j])
+                {
+                    ++i, ++j;
+                }
 
-            // the following condition implies that the current word of buffer
-            // is equal to input word
-            if ((i == n || buffer[i] == ' ' || buffer[i] == '\n') && j == m)
-            {
-                printf("Line: %d ", line);
-                printf("Column: %d\n", i - m);
-                count++;
+                // the following condition implies that the current word of buffer
+                // is equal to input word
+                if ((i == n || buffer[i] == ' ' || buffer[i] == '\n') && j == m)
+                {
+                    printf("target word: %s ", word);
+                    printf("Line: %d ", line);
+                    printf("Column: %d\n", i - m);
+                    count[index]++;
+                }
             }
 
             // moving to next word
@@ -58,6 +72,7 @@ int main()
 
         ++line;
     }
-    printf("there's %d \"%s\" in the file", count, wrd);
+    for (int i = 0; i < k; i++)
+        printf("there's %d \"%s\" in the file   \n", count[i], wrd[i]);
     fclose(fp);
 }
