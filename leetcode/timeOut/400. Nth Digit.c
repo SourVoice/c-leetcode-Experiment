4c8cf430fc1b13cd24c2201d698fe9985aceabad
SourVoice <2985122798@qq.com>
2021-12-02 00:10:13 +0800

    timeout

--- leetcode/timeOut/400. Nth Digit.c
+++ leetcode/timeOut/400. Nth Digit.c
@@ -3,7 +3,7 @@
 //³¬Ê±
 int findNthDigit(int n)
 {
-    long sum = 0; //rec the pos of all the nums
+    long sum = 0; // rec the pos of all the nums
     long numth = 0;
     for (long i = 1; i <= n; i++)
     {
@@ -13,11 +13,10 @@ int findNthDigit(int n)
             numth++;
             if (n <= sum)
             {
-                // printf("numth = %d, sum = %d\n",numth, sum);
                 return numth % (int)pow(10, sum - n + 1) / pow(10, sum - n);
             }
         }
     }
     return 0;
 }
-//1,3,4,5,6,7,8,9,10,11,12
+// 1,3,4,5,6,7,8,9,10,11,12
