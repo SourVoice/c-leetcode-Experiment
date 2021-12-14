9ada6d8e04bbdf602e1f7360cb395bcbaf8f1895
SourVoice <2985122798@qq.com>
2021-12-14 11:29:46 +0800

    后序遍历的迭代法

--- leetcode/590. N-ary Tree Postorder Traversal.c
+++ leetcode/590. N-ary Tree Postorder Traversal.c
@@ -55,3 +55,50 @@ int *postorder(struct Node *root, int *returnSize)
     ans[(*returnSize)++] = root->val;
     return ans;
 }
+
+/*
+迭代法,使用先序遍历后倒转顺序
+void size(struct Node *root, int *count)
+{
+    if (!root)
+        return;
+    for (int i = 0; i < root->numChildren; i++)
+    {
+        (*count)++;
+        size(root->children[i], count);
+    }
+}
+void reverse(int *arr, int arrSize)
+{
+    for (int i = 0, j = arrSize - 1; i <= j; i++, j--)
+    {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
+int *postorder(struct Node *root, int *returnSize)
+{
+    int treeSize = 1;
+    size(root, &treeSize);
+    int *ans = (int *)malloc(sizeof(int) * treeSize);
+    *returnSize = 0;
+    struct Node **stack = (struct Node **)malloc(sizeof(struct Node *) * treeSize);
+    if (!root)
+        return ans;
+    int top = -1;
+    stack[++top] = root;
+    while (top != -1)
+    {
+        root = stack[top--];
+        if (root)
+            ans[(*returnSize)++] = root->val;
+        for (int i = 0; i < root->numChildren; i++)
+            stack[++top] = root->children[i];
+    }
+    reverse(ans, *returnSize);
+    return ans;
+}
+
+
+*/
