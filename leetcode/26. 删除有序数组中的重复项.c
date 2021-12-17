11d7b4bb816bccf6bcd2f12e7fb070b3ecec8796
SourVoice <2985122798@qq.com>
2021-12-17 20:45:21 +0800

    add comment

--- leetcode/26. 删除有序数组中的重复项.c
+++ leetcode/26. 删除有序数组中的重复项.c
@@ -1,12 +1,12 @@
 int removeDuplicates(int *nums, int numsSize)
 {
     if (numsSize == 0)
-        return 0;
-    int left = 0;
+        return nums;
 
+    int left = 0;
     for (int right = 1; right < numsSize; right++)
     {
-        if (nums[left] != nums[right])
+        if (nums[left] != nums[right]) //当右不为左相同
         {
             left++;
             nums[left] = nums[right];
