90259d377b8a7b9c46dab5b5a5d3e3100e2af3df
SourVoice <2985122798@qq.com>
2021-12-17 21:26:44 +0800

    添加双指针法

--- leetcode/26. Remove Duplicates from Sorted Array.c
+++ leetcode/26. Remove Duplicates from Sorted Array.c
@@ -1,16 +1,37 @@
+// 26. 删除有序数组中的重复项
+
+//双指针
 int removeDuplicates(int *nums, int numsSize)
 {
-    if (numsSize == 0)
-        return nums;
-
-    int left = 0;
-    for (int right = 1; right < numsSize; right++)
+    if (!numsSize)
+        return 0;
+    int count = 0;
+    int *leftp = nums;
+    for (int *rightp = leftp + 1; rightp < nums + numsSize; rightp++)
     {
-        if (nums[left] != nums[right]) //当右不为左相同
+        if (*leftp != *rightp)
         {
-            left++;
-            nums[left] = nums[right];
+            count++;
+            leftp++;
+            *leftp = *rightp;
         }
     }
-    return left + 1;
+    return count + 1;
 }
+
+// int removeDuplicates(int *nums, int numsSize)
+// {
+//     if (numsSize == 0)
+//         return nums;
+
+//     int left = 0;
+//     for (int right = 1; right < numsSize; right++)
+//     {
+//         if (nums[left] != nums[right]) //当右不为左相同
+//         {
+//             left++;
+//             nums[left] = nums[right];
+//         }
+//     }
+//     return left + 1;
+// }
