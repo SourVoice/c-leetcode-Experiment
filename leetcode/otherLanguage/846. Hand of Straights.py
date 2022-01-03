282d251e513c2d84dc9194371a3897450a11327e
SourVoice <2985122798@qq.com>
2022-01-03 22:27:11 +0800

    add commemt

--- leetcode/otherLanguage/846. Hand of Straights.py
+++ leetcode/otherLanguage/846. Hand of Straights.py
@@ -10,8 +10,8 @@ class Solution:
         for x in hand:
             if cnt[x] == 0:
                 continue
-            for num in range(x, x+groupSize):
-                if cnt[num] == 0:
+            for num in range(x, x+groupSize):  # 该组范围
+                if cnt[num] == 0:  # 该组内不能形成顺子序列
                     return False
-                cnt[num] -= 1
+                cnt[num] -= 1  # 该组形成了有序序列,每个
         return True
