a02acd6b28c58a325b1719a93aceab56f7b4f357
SourVoice <2985122798@qq.com>
2022-01-04 23:48:42 +0800

    简单计算

---
+++ leetcode/1154. Day of the Year.c
@@ -0,0 +1,27 @@
+// 1154.一年中的第几天
+#include <stdbool.h>
+#include <stdlib.h>
+
+int leapYear(int year)
+{
+    if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+        return 1;
+}
+
+// date format is YYYY-MM-DD
+int dayOfYear(char *date)
+{
+    int year = atoi(date);      // atoi converts the strng argument str to an integer
+    int month = atoi(date + 5); // return 5th
+    int day = atoi(date + 8);
+    int amount[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    if (leapYear(year))
+        amount[1] = 29;
+
+    int ans = 0;
+    for (int i = 0; i < month - 1; i++)
+        ans += amount[i];
+
+    return ans + day;
+}
