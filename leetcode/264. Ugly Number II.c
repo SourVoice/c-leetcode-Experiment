f4a6f497f558d46afe63e180f6b8d971e6cacde8
SourVoice <2985122798@qq.com>
2022-01-15 03:05:50 +0800

    动态规划,优先队列(未写)

---
+++ leetcode/264. Ugly Number II.c
@@ -0,0 +1,26 @@
+// 264. 丑数 II
+//动态规划,三指针法
+int nthUglyNumber(int n)
+{
+    int *dp = (int *)calloc(n + 1, sizeof(int));
+    dp[1] = 1;
+
+    int p2 = 1;
+    int p3 = 1;
+    int p5 = 1;
+    for (int i = 0; i <= n; i++)
+    {
+        int num2 = dp[p2] * 2; //下一个丑数
+        int num3 = dp[p3] * 3; //
+        int num5 = dp[p5] * 5; //
+        dp[i] = fmin(fmin(num2, num3), num5);
+        if (dp[i] == num2) //更新三个指针
+            p2++;
+        if (dp[i] == num3)
+            p3++;
+        if (dp[i] == num5)
+            p5++;
+    }
+    return dp[n];
+}
+//优先队列
