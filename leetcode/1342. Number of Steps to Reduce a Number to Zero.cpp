7bda1052c6a87b7b7c986b35d9957184f8b9a2d2
SourVoice <2985122798@qq.com>
2022-01-31 16:45:16 +0800

    位运算;递归;Hamming Algorithm

--- leetcode/1342. Number of Steps to Reduce a Number to Zero.cpp
+++ leetcode/1342. Number of Steps to Reduce a Number to Zero.cpp
@@ -29,3 +29,52 @@ public:
         return num ? (num & 1) ? 1 + numberOfSteps(num - 1) : 1 + numberOfSteps(num >> 1) : 0;
     }
 };
+//Î»ÔËËã
+class Solution3
+{
+public:
+    int length(uint num)
+    {
+        uint clz = 0;
+        if ((num >> 16) == 0)
+        {
+            clz += 16;
+            num <<= 16;
+        }
+        if ((num >> 24) == 0)
+        {
+            clz += 8;
+            num <<= 8;
+        }
+        if ((num >> 28) == 0)
+        {
+            clz += 4;
+            num <<= 4;
+        }
+        if ((num >> 30) == 0)
+        {
+            clz += 2;
+            num <<= 2;
+        }
+        if ((num >> 31) == 0)
+        {
+            clz += 1;
+        }
+        return 32 - clz;
+    }
+
+    int count(int num)
+    {
+        num = (num & 0x55555555) + ((num >> 1) & 0x55555555);
+        num = (num & 0x33333333) + ((num >> 2) & 0x33333333);
+        num = (num & 0x0F0F0F0F) + ((num >> 4) & 0x0F0F0F0F);
+        num = (num & 0x00FF00FF) + ((num >> 8) & 0x00FF00FF);
+        num = (num & 0x0000FFFF) + ((num >> 16) & 0x0000FFFF);
+        return num;
+    }
+
+    int numberOfSteps(int num)
+    {
+        return num == 0 ? 0 : length(num) - 1 + count(num);
+    }
+};
