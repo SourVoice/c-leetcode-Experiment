e8d0ca3130d03aac772771987156f4139b933dd1
SourVoice <2985122798@qq.com>
2022-02-03 01:14:47 +0800

    哈希表双映射

---
+++ leetcode/290. Word Pattern.cpp
@@ -0,0 +1,45 @@
+// 290. µ¥´Ê¹æÂÉ
+#include <unordered_map>
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+class Solution
+{
+public:
+    bool wordPattern(string pattern, string s)
+    {
+        unordered_map<char, string> mp;
+
+        unordered_map<string, char> mp2;
+        unordered_map<char, string>::iterator it;
+        string word;
+        stringstream ss(s);
+        int pattern_len = pattern.length();
+        for (int i = 0; i < pattern_len; i++)
+        {
+            ss >> word;
+            if (i == 0)
+            {
+                mp[pattern[i]] = word;
+                mp2[word] = pattern[i];
+            }
+            else
+            {
+                if (mp.count(pattern[i]) || mp2.count(word))
+                {
+                    if (mp[pattern[i]] != word || mp2[word] != pattern[i])
+                        return false;
+                }
+                else
+                {
+                    mp[pattern[i]] = word;
+                    mp2[word] = pattern[i];
+                }
+            }
+        }
+        if (ss >> word)
+            return false;
+        return true;
+    }
+};
