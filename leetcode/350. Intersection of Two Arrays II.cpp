b3d927101c792f473475fe0458fe57b80b9c3003
SourVoice <2985122798@qq.com>
2022-02-11 22:08:07 +0800

    双指针

--- leetcode/350. Intersection of Two Arrays II.cpp
+++ leetcode/350. Intersection of Two Arrays II.cpp
@@ -74,3 +74,35 @@ public:
         return ans;
     }
 };
+
+//˫ָ�뷨
+class Solution3
+{
+public:
+    vector<int> intersect(vector<int> &nums1, vector<int> &nums2)
+    {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int length1 = nums1.size(), length2 = nums2.size();
+        vector<int> intersection;
+        int index1 = 0, index2 = 0;
+        while (index1 < length1 && index2 < length2)
+        {
+            if (nums1[index1] < nums2[index2])
+            {
+                index1++;
+            }
+            else if (nums1[index1] > nums2[index2])
+            {
+                index2++;
+            }
+            else
+            {
+                intersection.push_back(nums1[index1]);
+                index1++;
+                index2++;
+            }
+        }
+        return intersection;
+    }
+};
