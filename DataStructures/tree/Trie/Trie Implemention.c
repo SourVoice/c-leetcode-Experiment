d488169d737c181b974056dfc4eaf35d298a267c
SourVoice <2985122798@qq.com>
2022-03-19 00:10:57 +0800

    字典树c实现

---
+++ DataStructures/tree/Trie/Trie Implemention.c
@@ -0,0 +1,83 @@
+// Trie's implementation by c(×ÖµäÊ÷)
+#include <stdbool.h>
+#include <stdio.h>
+
+#define ALPHABET_SIZE 26 // next letter may contains most 26 alpha
+struct TrieNode
+{
+    struct TireNode *children[ALPHABET_SIZE];
+    bool isEndOfWord;
+};
+// retyrb new TrieNode
+struct TrieNode *getNode(void)
+{
+    struct TrieNode *pNode;
+    pNode = (struct TrieNode *)malloc(sizeof(struct TrieNode));
+    if (pNode)
+    {
+        int i;
+        for (i = 0; i < ALPHABET_SIZE; i++)
+        {
+            pNode->children[i] = NULL;
+        }
+    }
+    return pNode;
+}
+
+// insert a word into Trie
+void insert(struct TrieNode *root, const char *key)
+{
+    int level;
+    int length = strlen(key);
+    int index;
+    struct TrieNode *pCrawl = root;
+    for (level = 0; level < length; level++)
+    {
+        index = key[level] - 'a';
+        if (!pCrawl->children[index])
+            pCrawl->children[index] = getNode();
+        pCrawl = pCrawl->children[index];
+    }
+    // mark last node as leaf
+    pCrawl->isEndOfWord = true;
+}
+bool search(struct TrieNode *root, const char *key)
+{
+    int level;
+    int length = strlen(key);
+    int index;
+    struct TrieNode *pCrawl = root;
+    for (level = 0; level < length; level++)
+    {
+        index = key[level] - 'a';
+        if (!pCrawl->children[index])
+            return false;
+        pCrawl = pCrawl->children[index];
+    }
+    return pCrawl->isEndOfWord;
+}
+
+// Driver
+int main()
+{
+    // Input keys (use only 'a' through 'z' and lower case)
+    char keys[][8] = {"the", "a", "there", "answer", "any",
+                      "by", "bye", "their"};
+
+    char output[][32] = {"Not present in trie", "Present in trie"};
+
+    struct TrieNode *root = getNode();
+
+    // Construct trie
+    int i;
+    for (i = 0; i < ARRAY_SIZE(keys); i++)
+        insert(root, keys[i]);
+
+    // Search for different keys
+    printf("%s --- %s\n", "the", output[search(root, "the")]);
+    printf("%s --- %s\n", "these", output[search(root, "these")]);
+    printf("%s --- %s\n", "their", output[search(root, "their")]);
+    printf("%s --- %s\n", "thaw", output[search(root, "thaw")]);
+
+    return 0;
+}
