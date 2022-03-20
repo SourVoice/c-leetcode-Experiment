61ea18b0499f101144fa638a4a9dee3ce01b14f8
SourVoice <2985122798@qq.com>
2022-03-20 22:51:55 +0800

    easy

---
+++ leetcode/2043. Simple Bank System.cpp
@@ -0,0 +1,54 @@
+// 2043. 简易银行系统
+
+#include <vector>
+using namespace std;
+class Bank
+{
+public:
+    Bank(vector<long long> &balance)
+    {
+        this->balance = balance;
+    }
+
+    bool transfer(int account1, int account2, long long money)
+    {
+        if (account1 > 0 && account1 <= balance.size() && account2 > 0 && account2 <= balance.size() && balance[account1 - 1] >= money)
+        {
+            balance[account1 - 1] -= money;
+            balance[account2 - 1] += money;
+            return true;
+        }
+        return false;
+    }
+
+    bool deposit(int account, long long money)
+    {
+        if (account > 0 && account <= balance.size())
+        {
+            balance[account - 1] += money;
+            return true;
+        }
+        return false;
+    }
+
+    bool withdraw(int account, long long money)
+    {
+        if (account > 0 && account <= balance.size() && balance[account - 1] >= money)
+        {
+            balance[account - 1] -= money;
+            return true;
+        }
+        return false;
+    }
+
+private:
+    vector<long long> balance;
+};
+
+/**
+ * Your Bank object will be instantiated and called as such:
+ * Bank* obj = new Bank(balance);
+ * bool param_1 = obj->transfer(account1,account2,money);
+ * bool param_2 = obj->deposit(account,money);
+ * bool param_3 = obj->withdraw(account,money);
+ */
