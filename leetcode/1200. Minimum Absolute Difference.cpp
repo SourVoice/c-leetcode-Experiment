35cad93f5e043e72d677905b6a52b2eef659f387
SourVoice <2985122798@qq.com>
2022-07-04 14:43:10 +0800

    模拟;简单

---
+++ leetcode/1200. Minimum Absolute Difference.cpp
@@ -0,0 +1,25 @@
+// 1200. 最小绝对差
+#include <vector>
+using namespace std;
+// 模拟
+class Solution
+{
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int> &arr)
+    {
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+        int min = INT_MAX;
+        for (int i = 0; i < n - 1; i++)
+        {
+            min = fmin(min, arr[i + 1] - arr[i]);
+        }
+        vector<vector<int>> ret;
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (arr[i + 1] - arr[i] == min)
+                ret.push_back({arr[i], arr[i + 1]});
+        }
+        return ret;
+    }
+};
