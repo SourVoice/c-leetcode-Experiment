3b35cef2a60af0a38dd83999aea35dee8eb521db
SourVoice <2985122798@qq.com>
2022-07-13 21:11:26 +0800

    add comment

--- leetcode/122. Best Time to Buy and Sell Stock II.cpp
+++ leetcode/122. Best Time to Buy and Sell Stock II.cpp
@@ -7,6 +7,7 @@ public:
     int maxProfit(vector<int> &prices)
     {
         int ret = 0;
+        // 因为交易次数不受限，如果可以把所有的上坡全部收集到，一定是利益最大化的
         for (int i = 1; i < prices.size(); i++)
         {
             if (prices[i] > prices[i - 1])
