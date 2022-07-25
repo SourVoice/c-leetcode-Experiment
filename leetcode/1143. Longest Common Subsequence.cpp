ac7209694c897a21ced9b1f9654318bd3ba2849d
SourVoice <2985122798@qq.com>
2022-07-26 00:12:04 +0800

    动态规划

---
+++ leetcode/1143. Longest Common Subsequence.cpp
@@ -0,0 +1,32 @@
+// 1143. 最长公共子序列
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    int longestCommonSubsequence(string text1, string text2)
+    {
+        int m = text1.size();
+        int n = text2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0)); // DP[i][j] represents the longest common subsequence of text1[0 ... i] & text2[0 ... j].
+        for (int i = 1; i <= m; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (text1[i - 1] == text2[j - 1])
+                {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
