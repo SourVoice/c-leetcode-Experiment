06a203eedd94e1b0fd557c394eb043ffe3f85c85
SourVoice <2985122798@qq.com>
2022-08-10 23:39:52 +0800

    二分查找

--- leetcode/1385. Find the Distance Value Between Two Arrays.cpp
+++ leetcode/1385. Find the Distance Value Between Two Arrays.cpp
@@ -14,12 +14,14 @@ public:
         sort(arr2.begin(), arr2.end());
         int ans = 0;
         //在arr1中找到绝对值之差大于d的个数
-        for(int i = 0; i < arr1.size();i++){
+        for (int i = 0; i < arr1.size(); i++)
+        {
             bool flag = true;
-            for(auto &elem  : arr2) 
-                if(abs(arr1[i] - elem) <= d)
+            for (auto &elem : arr2)
+                if (abs(arr1[i] - elem) <= d)
                     flag = false;
-            if(flag) ans++;
+            if (flag)
+                ans++;
         }
         return ans;
     }
@@ -34,18 +36,22 @@ public:
         sort(arr1.begin(), arr1.end());
         int ans = 0;
         //在arr1中找到绝对值之差大于d的个数
-        for(int i = 0; i < arr1.size();i++){
+        for (int i = 0; i < arr1.size(); i++)
+        {
             bool flag = true;
-            int left= 0, right = arr2.size() - 1;
-            while(left <= right){
+            int left = 0, right = arr2.size() - 1;
+            while (left <= right)
+            {
                 int mid = (left - right) / 2 + right;
-                if(arr1[i] - arr2[mid] > d)
+                if (arr1[i] - arr2[mid] > d)
                     left = mid + 1;
-                else if(arr2[mid] - arr1[i] > d)
+                else if (arr2[mid] - arr1[i] > d)
                     right = mid - 1;
-                else break;
+                else
+                    break;
             }
-            if(left > right) ans++;//已经全部查找
+            if (left > right)
+                ans++; //已经全部查找
         }
         return ans;
     }
