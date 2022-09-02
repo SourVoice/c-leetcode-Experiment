1d66b00f816b102baec7cbba2ba435e74e1623d9
SourVoice <2985122798@qq.com>
2022-09-02 10:46:14 +0800

    栈模拟

---
+++ leetcode/946. Validate Stack Sequences.cpp
@@ -0,0 +1,27 @@
+// 946. —È÷§’ª–Ú¡–
+#include <stack>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    bool validateStackSequences(vector<int> &pushed, vector<int> &popped)
+    {
+        stack<int> st;
+        int j = 0;
+        for (int i = 0; i < pushed.size(); i++)
+        {
+            st.push(pushed[i]);
+            while (popped[j] == st.top())
+            {
+                st.pop();
+                j++;
+            }
+        }
+        return st.empty();
+    }
+};
