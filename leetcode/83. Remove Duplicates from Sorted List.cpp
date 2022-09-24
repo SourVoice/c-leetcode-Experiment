495d5bb9cfa7d7384fe9bce52d6a24d47eba6f2a
SourVoice <2985122798@qq.com>
2022-09-24 08:57:54 +0800

    链表模拟

---
+++ leetcode/83. Remove Duplicates from Sorted List.cpp
@@ -0,0 +1,38 @@
+// 83. 删除排序链表中的重复元素
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+using namespace std;
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution
+{
+public:
+    ListNode *deleteDuplicates(ListNode *head)
+    {
+        ListNode *cur = head;
+        while (cur && cur->next)
+        {
+            if (cur->val == cur->next->val)
+                cur->next = cur->next->next;
+            else
+                cur = cur->next;
+        }
+        return head;
+    }
+};
