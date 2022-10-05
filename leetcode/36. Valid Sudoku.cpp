46cd05bcb5e305346f026c732fd27deef4bb2610
SourVoice <2985122798@qq.com>
2022-10-05 17:49:18 +0800

    矩阵;行列关系

--- leetcode/36. Valid Sudoku.cpp
+++ leetcode/36. Valid Sudoku.cpp
@@ -36,3 +36,31 @@ public:
         return true;
     }
 };
+// 我的做法
+class Solution
+{
+public:
+    bool isValidSudoku(vector<vector<char>> &board)
+    {
+        vector<vector<int>> rows(9, vector<int>(9, 0));                                   // 统计row[i]行数字出现频率
+        vector<vector<int>> cols(9, vector<int>(9, 0));                                   // 统计col[i]列数字出现频率
+        vector<vector<vector<int>>> subMat(3, vector<vector<int>>(3, vector<int>(9, 0))); // submat[i][j]用于标注大方格的坐标
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                char cell = board[i][j];
+                int index = cell - '1';
+                if (cell != '.')
+                {
+                    rows[i][index]++;
+                    cols[j][index]++;
+                    subMat[i / 3][j / 3][index]++; // 大方格每三个出现一次
+                    if (rows[i][index] > 1 || cols[j][index] > 1 || subMat[i / 3][j / 3][index] > 1)
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+};
