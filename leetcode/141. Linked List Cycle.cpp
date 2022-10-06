2b166fd0989bf326f123e608803ddc1b746e412a
SourVoice <2985122798@qq.com>
2022-10-06 20:04:41 +0800

    哈希表;快慢指针

--- leetcode/141. Linked List Cycle.cpp
+++ leetcode/141. Linked List Cycle.cpp
@@ -1,13 +1,24 @@
-// 141. Linked List Cycle
-
+// 141. 环形链表
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <list>
+using namespace std;
 /**
  * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
  */
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
 //哈希表法
 class Solution
 {
@@ -15,31 +26,24 @@ public:
     bool hasCycle(ListNode *head)
     {
         set<ListNode *> st;
-        struct ListNode *curr = head;
-        while (curr && curr->next)
+        ListNode *cur = head;
+        while (cur && cur->next)
         {
-            if (st.count(curr))
+            if (st.count(cur))
                 return true;
-            st.insert(curr);
-            curr = curr->next;
+            st.insert(cur);
+            cur = cur->next;
         }
         return false;
     }
 };
 
 //快慢指针法
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
 bool hasCycle(struct ListNode *head)
 {
     struct ListNode *slow = head;
     struct ListNode *fast = head;
-    while (fast && fast->next)
+    while (fast && fast->next) // fast 先进行判断, 防止fast->next为UB
     {
         slow = slow->next;
         fast = fast->next->next;
