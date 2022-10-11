5b22a0b4efc8a7243e47d202b4342ea13c7e4b79
SourVoice <2985122798@qq.com>
2022-10-11 21:34:42 +0800

    取指针地址;链表;递归

---
+++ leetcode/24. Swap Nodes in Pairs.cpp
@@ -0,0 +1,61 @@
+// 24. 两两交换链表中的节点
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+using namespace std;
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+// 递归
+class Solution
+{
+public:
+    ListNode *swapPairs(ListNode *head)
+    {
+        if (head == nullptr || head->next == nullptr)
+            return head;
+        return helper(head);
+    }
+    ListNode *helper(ListNode *&head)
+    {
+        if (head == nullptr || head->next == nullptr)
+            return nullptr;
+        ListNode *node = new ListNode;
+        node = head->next;
+        head->next = swapPairs(node->next);
+        node->next = head;
+        return node;
+    }
+};
+// 循环做法
+class Solution
+{
+public:
+    ListNode *swapPairs(ListNode *head)
+    {
+        ListNode **shaobing = &head;
+        ListNode *a, *b;
+        while ((a = *shaobing) && (b = a->next))
+        {
+            a->next = b->next;
+            b->next = a;
+            *(shaobing) = b;
+            shaobing = &(a->next);
+        }
+        return head;
+    }
+};
