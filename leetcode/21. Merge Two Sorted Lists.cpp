7c87c35b2c572c8614371217ee455dc819c7d37f
SourVoice <2985122798@qq.com>
2022-10-14 17:57:21 +0800

    递归;链表模拟

--- leetcode/21. Merge Two Sorted Lists.cpp
+++ leetcode/21. Merge Two Sorted Lists.cpp
@@ -1,12 +1,76 @@
 // 21. 合并两个有序链表
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <malloc.h>
+using namespace std;
 /**
  * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
  */
-#include <malloc.h>
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+// 链表模拟
+class Solution
+{
+public:
+    ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
+    {
+        ListNode *result = new ListNode();
+        ListNode *head = result;
+        while (list1 && list2)
+        {
+            if (list1->val <= list2->val)
+            {
+                result->next = list1;
+                list1 = list1->next;
+            }
+            else
+            {
+                result->next = list2;
+                list2 = list2->next;
+            }
+            result = result->next;
+        }
+        result->next = list1 ? list1 : list2;
+        return head->next;
+    }
+};
+// 递归
+class Solution
+{
+public:
+    ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
+    {
+        if (!list1)
+            return list2;
+        if (!list2)
+            return list1;
+        if (list1->val <= list2->val)
+        {
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        }
+        list2->next = mergeTwoLists(list1, list2->next);
+        return list2;
+    }
+};
+// 以下为 原C语言题解
+// 链表模拟
+/**
+ * Definition for singly-linked list.
+ */
 struct ListNode
 {
     int val;
@@ -20,9 +84,7 @@ struct ListNode *mergeTwoLists(struct ListNode *l1, struct ListNode *l2)
     head->next = l1;
 
     if (!p)
-    {
         return NULL;
-    }
     while (l1 && l2)
     {
         if (l1->val <= l2->val)
