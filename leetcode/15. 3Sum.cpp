063734c87eafba410dcc4f52b735b8295483f462
SourVoice <2985122798@qq.com>
2022-10-15 09:08:38 +0800

    更新双指针写法

--- leetcode/15. 3Sum.cpp
+++ leetcode/15. 3Sum.cpp
@@ -99,3 +99,38 @@ public:
         return ans;
     }
 };
+// ¸üÐÂÐ´·¨
+class Solution
+{
+public:
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
+        int n = nums.size();
+        sort(begin(nums), end(nums));
+        vector<vector<int>> ans;
+        for (int i = 0; i < n - 2; i++)
+        {
+            int tar = 0 - nums[i];
+            int j = i + 1, k = n - 1;
+            while (j < k)
+            {
+                if (nums[j] + nums[k] > tar)
+                    k--;
+                else if (nums[j] + nums[k] < tar)
+                    j++;
+                else if (nums[j] + nums[k] == tar)
+                {
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    while (j < k && nums[j] == nums[j + 1])
+                        j++;
+                    while (j < k && nums[k] == nums[k - 1])
+                        k--;
+                    j++, k--;
+                }
+            }
+            while (i < n - 2 && nums[i] == nums[i + 1])
+                i++;
+        }
+        return ans;
+    }
+};
