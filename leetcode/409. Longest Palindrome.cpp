324e583fbd8c0dd60fc678672821d9e2192dd275
SourVoice <2985122798@qq.com>
2022-10-18 15:45:48 +0800

    哈希表;二刷

--- leetcode/409. Longest Palindrome.cpp
+++ leetcode/409. Longest Palindrome.cpp
@@ -1,5 +1,14 @@
 // 409. 最长回文串
-
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+using namespace std;
 // hash表
 class Solution
 {
@@ -9,20 +18,35 @@ public:
         int res = 0;
         unordered_map<char, int> m;
         for (auto c : s)
-        {
             m[c]++;
-        }
         for (auto it : m)
         {
             if (it.second % 2 == 0)
-            {
                 res += it.second;
-            }
             else
-            {
                 res += it.second - 1;
-            }
         }
         return res >= s.size() ? res : res + 1;
+    }
+};
+// 哈希表
+class Solution
+{
+public:
+    int longestPalindrome(string s)
+    {
+        unordered_map<char, int> mp;
+        for (auto &ch : s)
+            mp[ch]++;
+        int ans = 0;
+        bool Odd = false;
+        for (auto &[ch, t] : mp)
+        {
+            if (t & 1)
+                ans += t - 1, Odd = true;
+            else
+                ans += t;
+        }
+        return ans + Odd;
     }
 };
