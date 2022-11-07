45481b24ea263ad3b9b067f9ac90fb22f6e942be
SourVoice <2985122798@qq.com>
2022-11-07 19:59:51 +0800

    回溯;dfs;二刷

--- leetcode/77. Combinations;.cpp
+++ leetcode/77. Combinations;.cpp
@@ -1,5 +1,17 @@
 // 77. 组合
-
+#include <string>
+#include <deque>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <queue>
+using namespace std;
 // 回溯法
 class Solution
 {
@@ -25,4 +37,34 @@ public:
             gen.pop_back();
         }
     }
+};
+// 二刷
+// dfs
+class Solution
+{
+public:
+    vector<vector<int>> combine(int n, int k)
+    {
+        this->totalNum = n;
+        dfs(1, k);
+        return ret;
+    }
+    void dfs(int index, int k)
+    {
+        if (gen.size() == k)
+        {
+            ret.push_back(gen);
+        }
+        for (int i = index; i <= totalNum; i++)
+        {
+            gen.push_back(i);
+            dfs(i + 1, k);
+            gen.pop_back();
+        }
+    }
+
+private:
+    int totalNum;
+    vector<vector<int>> ret;
+    vector<int> gen; // 记录一次的组合
 };
