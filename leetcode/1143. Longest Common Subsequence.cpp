8fa9838c070a989ea5370dd60e3c5a52232176dc
SourVoice <2985122798@qq.com>
2022-11-26 20:47:37 +0800

    动态规划;二维dp

--- leetcode/1143. Longest Common Subsequence.cpp
+++ leetcode/1143. Longest Common Subsequence.cpp
@@ -30,3 +30,24 @@ public:
         return dp[m][n];
     }
 };
+// 动态规划
+class Solution
+{
+public:
+    int longestCommonSubsequence(string text1, string text2)
+    {
+        int m = text1.size(), n = text2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0)); // 代表0-i和0-j公共子序列的最长长度
+        for (int i = 1; i <= m; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (text1[i - 1] == text2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[m][n];
+    }
+};
