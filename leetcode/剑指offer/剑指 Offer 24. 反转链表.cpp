4c7e415b636688fe2c541e0ddbf73c5fbf9e0c9d
SourVoice <2985122798@qq.com>
2023-01-02 23:19:12 +0800

    链表

---
+++ leetcode/剑指offer/剑指 Offer 24. 反转链表.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <stack>
+#include <functional>
+#include <deque>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <queue>
+#include <limits.h>
+#include <stdbool.h>
+#include <math.h>
+using namespace std;
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        if (!head->next)
+            return head;
+        ListNode *node = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        return node;
+    }
+};
