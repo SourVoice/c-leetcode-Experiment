5c2374645811e43f1f1ed1454bcda8c2ff6de2f3
SourVoice <2985122798@qq.com>
2023-01-02 22:52:29 +0800

    链表;递归

---
+++ leetcode/剑指offer/剑指 Offer 06. 从尾到头打印链表.cpp
@@ -0,0 +1,51 @@
+#include <string>
+#include <stack>
+#include <functional>
+#include <deque>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <queue>
+#include <limits.h>
+#include <stdbool.h>
+#include <math.h>
+using namespace std;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+class Solution
+{
+public:
+    vector<int> reversePrint(ListNode *head)
+    {
+        helper(head);
+        return vec;
+    }
+    void helper(ListNode *head)
+    {
+        if (!head)
+            return;
+        helper(head->next);
+        vec.push_back(head->val);
+    }
+
+private:
+    vector<int> vec;
+};
