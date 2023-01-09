d1f96974d67bc43ea2234e8a4a6938ab2ccc0531
SourVoice <2985122798@qq.com>
2023-01-09 22:24:03 +0800

    递归;二叉树

---
+++ leetcode/剑指offer/剑指 Offer 27. 二叉树的镜像.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <stack>
+#include <functional>
+#include <deque>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <queue>
+#include <limits.h>
+#include <stdbool.h>
+#include <math.h>
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution
+{
+public:
+    TreeNode *mirrorTree(TreeNode *root)
+    {
+        if (!root)
+            return nullptr;
+        TreeNode *node;
+        node = mirrorTree(root->left);
+        root->left = mirrorTree(root->right);
+        root->right = node;
+        return root;
+    }
+};
