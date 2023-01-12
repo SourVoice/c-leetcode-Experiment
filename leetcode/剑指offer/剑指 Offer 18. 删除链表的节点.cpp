9f48e6a23a4ca5f663b94a95dd57b8be5641e0a6
SourVoice <2985122798@qq.com>
2023-01-12 22:38:19 +0800

    递归

---
+++ leetcode/剑指offer/剑指 Offer 18. 删除链表的节点.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <stack>
+#include <functional>
+#include <deque>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <queue>
+#include <limits.h>
+#include <stdbool.h>
+#include <math.h>
+using namespace std;
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+class Solution
+{
+public:
+    ListNode *deleteNode(ListNode *head, int val)
+    {
+        if (head->val == val)
+            return head->next;
+        head->next = deleteNode(head->next, val);
+        return head;
+    }
+};
