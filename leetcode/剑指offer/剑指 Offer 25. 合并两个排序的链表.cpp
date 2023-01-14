6470945fc41faf06b1afe94e6bd2698592dbba3d
SourVoice <2985122798@qq.com>
2023-01-14 19:16:13 +0800

    链表;递归

---
+++ leetcode/剑指offer/剑指 Offer 25. 合并两个排序的链表.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <stack>
+#include <functional>
+#include <deque>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <queue>
+#include <limits.h>
+#include <stdbool.h>
+#include <math.h>
+using namespace std;
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+class Solution
+{
+public:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2)
+    {
+        if (!l1)
+            return l2;
+        if (!l2)
+            return l1;
+        if (l1->val <= l2->val)
+        {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        }
+        l2->next = mergeTwoLists(l1, l2->next);
+        return l2;
+    }
+};
