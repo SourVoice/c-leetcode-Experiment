97d56fc729807a8d42c28f5255bf046990ad6ca0
SourVoice <2985122798@qq.com>
2023-01-21 00:06:38 +0800

    递归;二叉搜索树

---
+++ leetcode/剑指offer/剑指 Offer 68 - I. 二叉搜索树的最近公共祖先.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <stack>
+#include <functional>
+#include <deque>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <queue>
+#include <limits.h>
+#include <stdbool.h>
+#include <math.h>
+#include <sstream>
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution
+{
+public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
+        if (root == p || root == q || !root)
+            return root;
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
+        if (left && right)
+            return root;
+        if (left && !right)
+            return left;
+        else if (!left && right)
+            return right;
+        else
+            return nullptr;
+    }
+};
