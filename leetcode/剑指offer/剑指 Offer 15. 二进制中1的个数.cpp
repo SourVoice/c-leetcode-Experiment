4fb1e14f330456969e740240f7a85754cf9ee530
SourVoice <2985122798@qq.com>
2023-01-23 21:59:17 +0800

    汉明码;位运算

---
+++ leetcode/剑指offer/剑指 Offer 15. 二进制中1的个数.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <stack>
+#include <functional>
+#include <deque>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <queue>
+#include <limits.h>
+#include <stdbool.h>
+#include <math.h>
+#include <sstream>
+using namespace std;
+class Solution
+{
+public:
+    int hammingWeight(uint32_t n)
+    {
+        int ret =0;
+        while(n){
+            n&=(n -1), ret++;
+        }
+        return ret;
+    }
+};
